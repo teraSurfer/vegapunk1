92dbd16bd4dad17ae4b475c997d61f7d2aac9282
Replit user <>
2022-12-30 03:05:58 +0000

    discord bot

---
+++ index.js
@@ -0,0 +1,67 @@
+const { Client, GatewayIntentBits, Collection, Events } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+
+  ]
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
+});
+
+client.login(process.env['TOKEN']);
+
+app.get('/', async (req, res, next) => {
+  res.status(200).json({ message: 'OK' });
+});
+
+app.get('/twitch', async (req, res, next) => {
+  res.status(200).json({ message: 'OK' });
+});
+
+app.listen(8080, '0.0.0.0', () => {
+  console.log('Started bot server');
+});
